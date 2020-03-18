54b80b76dacc8cdd0e3ab18b044b684159d2abe9
Jake Hamilton <jakehamtexas@gmail.com>
2020-03-18 13:50:56 -0500

    Simplifies debug stuff for great justice

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,3 @@
-import Express from "express";
 import { MetalNewsManager } from "./manager";
-const app: Express.Application = Express();
-const port: number = 8080;
 
-app.set("port", port);
-
-app.get("/", async req => {
-  const { quantity } = req.body;
-  const {
-    heavyBlogIsHeavy: { getTopN }
-  } = MetalNewsManager;
-  const topN = await getTopN(quantity || 5);
-  console.log(topN);
-});
-
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
-});
+MetalNewsManager.heavyBlogIsHeavy.getTopN(5).then(console.log);
