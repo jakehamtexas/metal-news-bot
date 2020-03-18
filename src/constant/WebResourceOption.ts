98cfcbe101d8196e280d51b0e40d83dcd389848d
Jake Hamilton <jakehamtexas@gmail.com>
2020-03-18 16:07:00 -0500

    Some constants, not sure if I'm using these right quite yet.

---
+++ src/constant/WebResourceOption.ts
@@ -0,0 +1,3 @@
+export enum WebResourceOption {
+  HeavyBlogIsHeavy = "hbih"
+}
---
+++ src/constant/index.ts
@@ -0,0 +1,4 @@
+import { WebResourceOption } from "./WebResourceOption";
+import { BotCommand } from "./BotCommand";
+
+export { BotCommand, WebResourceOption };
