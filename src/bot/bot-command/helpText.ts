755ccf6cae2bda14d0a214fb9c7f70cb60361117
Jake Hamilton <jakehamtexas@gmail.com>
2020-03-20 14:22:13 -0500

    Heroku testing

--- src/bot/bot-command/helpText.ts
+++ src/bot/bot-command/helpText.ts
@@ -1,11 +1,13 @@
 import { Message } from "discord.js";
 
-const helpText = (
-  { mentions: { users }, content, channel: { send } }: Message,
-  botId: string
-) => {
-  if (users.has(botId) && content.includes("help")) {
-    send(`metal-news-bot
+const helpText = (message: Message, botId: string) => {
+  console.log(message.content);
+  if (
+    message.mentions &&
+    message.mentions.users.has(botId) &&
+    message.content.includes("help")
+  ) {
+    message.channel.send(`metal-news-bot
 Commands:
 \`@metal-news-bot help\` prints this help text
 \`!top <n> [hbih]\` gets the latest n headlines from any registered blog.
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ const logger = console;
 import getMessageHandler from "./bot/messageHandler";
 
 const bot = new Client();
-
+console.log(auth.botToken);
 bot.login(auth.botToken).then(_ => {
   bot.on("ready", () => {
-    logger.info("Connected");
-    logger.info("Logged in as:", bot.user?.tag, bot.user?.id);
+    logger.log("Connected");
+    logger.log("Logged in as:", bot.user?.tag, bot.user?.id);
   });
   bot.on("message", getMessageHandler(bot.user?.id));
 });
